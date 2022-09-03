28748531ece9fca71d0bc087fe539b8f1a94c1ca
xdarov <xdarovv@gmail.com>
2022-09-03 11:15:02 +0300

    cpp04

--- CPPModule04/ex01/main.cpp
+++ CPPModule04/ex01/main.cpp
@@ -3,7 +3,7 @@
 
 int main ()
 {
-    Animal *animals[6];
+    const Animal *animals[6];
     int i = -1;
 
     while (++i < 3)
@@ -12,15 +12,20 @@ int main ()
         animals[i++] = new Cat();
     
     std::cout << "---------------------" << std::endl;
-    
+    animals[5]->set_ideas("'bite the dog'");
+    animals[5]->set_ideas("'purr'");
+    animals[5]->set_ideas("'go eat'");
+    animals[5]->show_idea();
 
+    animals[0]->set_ideas("'barking'");
+    animals[0]->set_ideas("'eat cat'");
+    animals[0]->set_ideas("'sleep'");
+    animals[0]->show_idea();
     std::cout << "---------------------" << std::endl;
 
     i = -1;
     while(++i < 6)
-    {
         delete animals[i];
-    }
 
     return 0;
 }    
---
+++ CPPModule04/ex02/main.cpp
@@ -0,0 +1,15 @@
+#include "Cat.hpp"
+#include "Dog.hpp"
+
+int main ()
+{
+    // Animal animal;
+    Dog dog;
+    Cat cat;
+
+    cat.makeSound();
+    dog.makeSound();
+
+
+    return 0;
+}    
